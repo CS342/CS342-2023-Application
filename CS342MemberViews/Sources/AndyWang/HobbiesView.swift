b51d6205c2420be8393dd1bc8cf245f06490a967
Andy <andrew.t.wang14@gmail.com>
2023-01-24 13:53:20 -0800

    Fix line lengths

--- CS342MemberViews/Sources/AndyWang/HobbiesView.swift
+++ CS342MemberViews/Sources/AndyWang/HobbiesView.swift
@@ -12,17 +12,17 @@ struct HobbiesView: View {
     var body: some View {
         ScrollView(.vertical) {
             VStack(spacing: 30) {
-                Text("- I have been playing saxophone for 8 years! At Stanford, I am the alto saxophonist for the Stanford Saxophone Quartet, and we host quarterly recitals and performances on campus.")
+                Text("- I am the alto saxophonist for the Stanford Saxophone Quartet, and we host quarterly recitals and performances on campus.")
                 saxophoneImage
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(maxWidth: 300)
-                Text("- I also enjoy playing volleyball! It is definitely the most fun sport out there, and there are so many variations to the game: beach, indoors, grass, doubles, etc. Currently I play on the Stanford Club Men's Volleyball Team as an outside hitter, and we compete against other collegiate club teams in the bay area.")
+                Text("- I also enjoy playing volleyball! Currently I play on the Stanford Club Men's Volleyball Team as an outside hitter.")
                 volleyballImage
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(maxWidth: 300)
-                Text("- A more minor hobby of mine is video and audio editing! I use Adobe Premiere for video and Audacity for audio. My knowledge of both are pretty rudimentary, and I mainly use these software for small personal projects here and there.")
+                Text("- A more minor hobby of mine is video editing! I use Adobe Premiere for small personal projects here and there.")
                 premiereImage
                     .resizable()
                     .aspectRatio(contentMode: .fit)
