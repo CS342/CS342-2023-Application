b7b90e52f5141ca71171b054520e62896cac21ce
parthav17 <parthavshergill@gmail.com>
2023-01-24 12:30:40 -0800

    Push Parthav's view to remote

---
+++ CS342MemberViews/Sources/ParthavShergill/MapView.swift
@@ -0,0 +1,27 @@
+//
+// This source file is part of the CS342 2023 Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 12.9716, longitude: 77.5946),
+        span: MKCoordinateSpan(latitudeDelta: 0.25, longitudeDelta: 0.25)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
