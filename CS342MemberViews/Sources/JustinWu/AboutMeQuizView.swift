578af510d2bf38dbbdfc2127d5f4aaa668dad204
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-01-28 00:49:55 -0500

    Add missing REUSE header

--- CS342MemberViews/Sources/JustinWu/AboutMeQuizView.swift
+++ CS342MemberViews/Sources/JustinWu/AboutMeQuizView.swift
@@ -1,3 +1,11 @@
+//
+// This source file is part of the CS342 2023 Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
 import SwiftUI
 import UIKit
 
