22c82ccdb216dd6a18f79eb892106f206fee036f
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-01-27 20:50:47 -0500

    clean up photos file

--- CS342MemberViews/Sources/AudreyLin/Dropdowns/Photos.swift
+++ CS342MemberViews/Sources/AudreyLin/Dropdowns/Photos.swift
@@ -13,66 +13,34 @@ import SwiftUI
 struct PhotosView: View {
     @State private var showDetail = false
     
-    
+
+    let imageArray = ["IMG_6406", "IMG_6391", "IMG_6356", "IMG_6320", "IMG_6269", "IMG_6205", "IMG_6099",
+                      "IMG_5879", "IMG_5871", "IMG_5401"]
+
     var body: some View {
         ZStack {
             Color("Gray").edgesIgnoringSafeArea(.all)
             ScrollView(.horizontal) {
                 HStack(spacing: 20) {
-                    Bundle.module.image(fromFileNamed: "IMG_6406")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                    Bundle.module.image(fromFileNamed: "IMG_6391")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                    Bundle.module.image(fromFileNamed: "IMG_6356")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                    Bundle.module.image(fromFileNamed: "IMG_6320")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                    Bundle.module.image(fromFileNamed: "IMG_6269")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                    Bundle.module.image(fromFileNamed: "IMG_6205")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                    Bundle.module.image(fromFileNamed: "IMG_6099")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                    Bundle.module.image(fromFileNamed: "IMG_5879")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                    Bundle.module.image(fromFileNamed: "IMG_5871")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                    Bundle.module.image(fromFileNamed: "IMG_5401")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 300)
-                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+
+                    ForEach(imageArray, id: \.self) { image in
+                        Bundle.module.image(fromFileNamed: image)
+                            .resizable()
+                            .modifier(ImageOptions())
+                    }
                 }
                     .padding(.vertical)
             }
         }
+    }
+}
+
+struct ImageOptions: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .scaledToFit()
+            .frame(height: 300)
+            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
     }
 }
 
